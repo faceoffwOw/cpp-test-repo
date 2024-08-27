138f520543a020d274e494882713f535568da1e7
faceoffwOw <get_right101@mail.ru>
2024-08-27 21:39:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@ int main()
     std::cout << "Hello, Git!" << std::endl;
     std::cout << "Test GitHub Desktop!" << std::endl;
     std::cout << "Test Clion!" << std::endl;
-    std::cout << "Test git fetch!" << std::endl;
+    std::cout << "Test git pull!" << std::endl;
 }
